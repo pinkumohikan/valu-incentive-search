58f28773f8e764324bf6ab1fbc4d61b64492cded
Hokuto Shinoda <pinkumohikan@gmail.com>
2018-05-03 17:31:08 +0900

    chore(migration): fix typo

--- database/migrations/2018_05_03_000000_create_initial_tables.php
+++ database/migrations/2018_05_03_000000_create_initial_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateIntialTables extends Migration
+class CreateInitialTables extends Migration
 {
     public function up()
     {
